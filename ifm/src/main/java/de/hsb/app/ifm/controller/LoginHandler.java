4db74ae6fa6e34354de0ad5a072945712d3582fd
Marius Schmitz <mschmitz@studenten-hs.bremerhaven.de>
2021-03-16 14:17:08 +0100

    LoginHandler hinzugefügt

--- ifm/src/main/java/de/hsb/app/ifm/controller/LoginHandler.java
+++ ifm/src/main/java/de/hsb/app/ifm/controller/LoginHandler.java
@@ -1,14 +1,14 @@
 package de.hsb.app.ifm.controller;
 
-
-
 import java.lang.module.Configuration;
 import java.sql.ResultSet;
 
 import java.sql.SQLException;
+import java.util.Iterator;
 import java.util.List;
 
 import javax.enterprise.context.RequestScoped;
+import javax.faces.model.DataModel;
 import javax.faces.model.ListDataModel;
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
@@ -20,115 +20,74 @@ import org.apache.log4j.Logger;
 import org.h2.engine.Session;
 import org.hibernate.SessionFactory;
 import org.hibernate.annotations.NamedQuery;
+import de.hsb.app.ifm.model.Benutzer;
 
-
-@Table(name="Benutzer")
-@NamedQuery(name="checkBenutzer",query="Select b from Benutzer b")
+@Table(name = "Benutzer")
+@NamedQuery(name = "checkBenutzer", query = "Select b from Benutzer b")
 @RequestScoped
 public class LoginHandler {
-	
-	@PersistenceContext(name="ifm-persistence-unit")
+
+	@PersistenceContext(name = "ifm-persistence-unit")
 	private EntityManager em;
-	
-	
+	private boolean loggedIn = false;
 	public EntityManager getEm() {
 		return em;
 	}
-
-
 
 	public void setEm(EntityManager em) {
 		this.em = em;
 	}
 
-
 //	private static Logger log = Logger.getLogger(LoginHandler.class);
 	private String username;
 	private String password;
 	private ResultSet rs;
-	
-	
+
 	public String getUsername() {
 		return username;
 	}
-
-
 
 	public void setUsername(String username) {
 		this.username = username;
 	}
 
-
-
 	public String getPassword() {
 		return password;
 	}
-
-
 
 	public void setPassword(String password) {
 		this.password = password;
 	}
 
-
 	@Transactional
-	public boolean login(String username, String password) {
-	//	Query checkuser = em.createQuery("Select username, password from Benutzer");
-	//	String dbUsername, dbPassword;
-		boolean login = false;
-		
-		List <?> list =  em.createNativeQuery("Select b from Benutzer b where b.username="+ username +" and b.password="+ password).
-				setParameter("username", username).
-				setParameter("password", password).
-				getResultList();
-				//	Query query = em.createNativeQuery("Select b from Benutzer b where b.username="+ username +" and b.password="+ password);
-		
-	
-		
-	//	List<?> list = query.getResultList();
-		
-		
-		
-		if (list.size() == 1) {
-			login = true;
-		} else if (list.size() == 0) {
-			login = false;
-		} 
-		
-	//	return (boolean) query.getSingleResult();
-		System.out.println(1);
-		System.out.println(list); 
-		System.out.println(login);
-		return login;
-		
+	public String login(DataModel<Benutzer> benutzer) {
+		// Query checkuser = em.createQuery("Select username, password from Benutzer");
+		// String dbUsername, dbPassword;
+		if(benutzer != null) {
+			for(Iterator <Benutzer> it = benutzer.iterator(); it.hasNext();) {
+				Benutzer nutzer = it.next();
+				if(username.equals(nutzer.getUsername()) && password.equals(nutzer.getPassword())) {
+					loggedIn = true;
+				}
+			}
+		}
+		if (loggedIn) {
+			return "index";
+		} else {
+			return "login";
+		}
 	}
 
-
-
 	public String logout() {
-		return "login";
+		loggedIn = false;
+		return "index";
 	}
 
 	public String checkLoggedIn() {
 		return "";
 	}
 
-
 }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 
 //list.forEach( -> Sysytem.out.println(.name));
 // Für den LoginHandler bzw. login brauche ich definitiv em / createNamedQuery oder createQuery / getSingleResult und unter umständen Resultlist
