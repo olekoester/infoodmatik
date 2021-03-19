dc8f34039bc92a6e5b2c5fe2ee4544b38fcb3348
Marius Schmitz <mschmitz@studenten-hs.bremerhaven.de>
2021-03-19 11:01:35 +0100

    Kommentare entfernt

--- ifm/src/main/java/de/hsb/app/ifm/controller/LoginHandler.java
+++ ifm/src/main/java/de/hsb/app/ifm/controller/LoginHandler.java
@@ -1,24 +1,13 @@
 package de.hsb.app.ifm.controller;
 
-import java.lang.module.Configuration;
 import java.sql.ResultSet;
-
-import java.sql.SQLException;
 import java.util.Iterator;
-import java.util.List;
-
 import javax.enterprise.context.RequestScoped;
 import javax.faces.model.DataModel;
-import javax.faces.model.ListDataModel;
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
-import javax.persistence.Query;
 import javax.persistence.Table;
 import javax.transaction.Transactional;
-
-import org.apache.log4j.Logger;
-import org.h2.engine.Session;
-import org.hibernate.SessionFactory;
 import org.hibernate.annotations.NamedQuery;
 import de.hsb.app.ifm.model.Benutzer;
 
@@ -38,7 +27,6 @@ public class LoginHandler {
 		this.em = em;
 	}
 
-//	private static Logger log = Logger.getLogger(LoginHandler.class);
 	private String username;
 	private String password;
 	private ResultSet rs;
@@ -61,8 +49,6 @@ public class LoginHandler {
 
 	@Transactional
 	public String login(DataModel<Benutzer> benutzer) {
-		// Query checkuser = em.createQuery("Select username, password from Benutzer");
-		// String dbUsername, dbPassword;
 		if(benutzer != null) {
 			for(Iterator <Benutzer> it = benutzer.iterator(); it.hasNext();) {
 				Benutzer nutzer = it.next();
@@ -91,8 +77,4 @@ public class LoginHandler {
 		}
 		return "index";
 	}
-
 }
-
-//list.forEach( -> Sysytem.out.println(.name));
-// Für den LoginHandler bzw. login brauche ich definitiv em / createNamedQuery oder createQuery / getSingleResult und unter umständen Resultlist
