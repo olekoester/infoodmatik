fe58fd0542241a8878da25fecf848d94c1dc4540
Schlauben <78811649+Schlauben@users.noreply.github.com>
2021-03-22 15:16:30 +0100

    Rezpterstellen nur noch eingeloggt erreichbar

--- ifm/src/main/java/de/hsb/app/ifm/controller/LoginHandler.java
+++ ifm/src/main/java/de/hsb/app/ifm/controller/LoginHandler.java
@@ -1,8 +1,5 @@
 package de.hsb.app.ifm.controller;
 
-
-
-
 import java.util.Iterator;
 import javax.enterprise.context.RequestScoped;
 import javax.faces.application.ConfigurableNavigationHandler;
@@ -13,7 +10,6 @@ import javax.persistence.PersistenceContext;
 import javax.persistence.Table;
 import javax.servlet.http.HttpSession;
 import javax.transaction.Transactional;
-
 
 import org.hibernate.annotations.NamedQuery;
 import de.hsb.app.ifm.model.Benutzer;
@@ -26,8 +22,7 @@ public class LoginHandler {
 	@PersistenceContext(name = "ifm-persistence-unit")
 	private EntityManager em;
 	private boolean loggedIn = false;
-	
-	
+
 	public EntityManager getEm() {
 		return em;
 	}
@@ -79,9 +74,9 @@ public class LoginHandler {
 		System.out.println("Hallo von Logout");
 		FacesContext fc = FacesContext.getCurrentInstance();
 		HttpSession session = (HttpSession) fc.getExternalContext().getSession(false);
-		if(session != null) {
+		if (session != null) {
 			session.invalidate();
-		}else {
+		} else {
 			System.out.println("Fehler beim Logout");
 		}
 		return "index";
@@ -93,27 +88,33 @@ public class LoginHandler {
 		FacesContext fc = FacesContext.getCurrentInstance();
 		HttpSession session = (HttpSession) fc.getExternalContext().getSession(false);
 		ConfigurableNavigationHandler nav = (ConfigurableNavigationHandler) fc.getApplication().getNavigationHandler();
-		if (session != null && session.getAttribute("id") != null){
-			if(session.getAttribute("id").equals(session.getId())){
+		if (checkLogin) {
+			if (session == null || session.getAttribute("id") == null) {
 				nav.performNavigation("index");
 			}
+		} else {
+			if (session != null && session.getAttribute("id") != null) {
+				if (session.getAttribute("id").equals(session.getId())) {
+					nav.performNavigation("index");
+				}
+			}
+
 		}
 	}
-	
-	
+
 	public boolean checkLogin() {
 		System.out.println("checkLogin");
 		System.out.println(loggedIn);
 		FacesContext fc = FacesContext.getCurrentInstance();
 		HttpSession session = (HttpSession) fc.getExternalContext().getSession(false);
 		ConfigurableNavigationHandler nav = (ConfigurableNavigationHandler) fc.getApplication().getNavigationHandler();
-		if (session != null && session.getAttribute("id") != null){
-			if(session.getAttribute("id").equals(session.getId())){
+		if (session != null && session.getAttribute("id") != null) {
+			if (session.getAttribute("id").equals(session.getId())) {
 				return true;
-			}else {
+			} else {
 				return false;
 			}
-		}else {
+		} else {
 			return false;
 		}
 	}
